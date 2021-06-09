453bc91835235ef156278bc31a4eee2b5d23f0cc
Mike Crowe <drmikecrowe@gmail.com>
2021-06-09 06:51:40 -0400

    Adding helper functions.  Fixing .gitconfig

--- .bash/custom/functions.bash
+++ .bash/custom/functions.bash
@@ -3,3 +3,24 @@
 mcd() {
     [ -n "$1" ] && mkdir -p "$@" && cd "$1";
 }
+
+makeIndex() {
+    if [ ! -z $1 ]; then
+        pushd $1
+    fi
+    find . -maxdepth 1 -type f -name '*.ts' | egrep -v "index|spec" | sort | sed "s/\.ts$/'/" | sed "s/^/export * from '/" > index.ts
+    find . -maxdepth 1 -type d | sort | while read DIR; do
+        if [ "$DIR" == "." ]; then continue; fi
+        if [ -f $DIR/index.ts ]; then
+            echo "export * from '$DIR'" >> index.ts
+        fi
+    done
+    if [ ! -z $1 ]; then
+        popd
+    fi
+}
+
+rsyncNode() {
+    mkdir -p "$2"
+    rsync -avrmR --exclude='node_modules/' --exclude='.tmp/' --exclude='.git/' --exclude='.webpack/' --exclude='.serverless/' $@
+}
